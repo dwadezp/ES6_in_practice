bdba2ec0c5e6fc07454ad2b13c895b0dab281e91
dwadezp@gmail.com <dwadezp@gmail.com>
2016-04-14 18:05:45 +0300

    Ready for traning

--- js/Element.js
+++ js/Element.js
@@ -1,13 +1,3 @@
 class Element {
-  constructor (name, template) {
-    this.name = name;
-    this.template = template;
-  }
-  draw (parentElement) {
-    var $element = $(this.template);
-    $element.attr("name", this.name);
-    $(parentElement).prepend($element);
-    return $element;
-  }
 }
 export default Element;
--- js/app.js
+++ js/app.js
@@ -1,9 +1,3 @@
 import Element from './Element.js';
-import InputElement from './InputElement.js';
 
-let testForm = new Element("form", "<form></form>");
-let testInput = new InputElement("password", "<input></input>", "password");
-debugger;
-testForm.draw("body");
-testInput.draw("form");
 
